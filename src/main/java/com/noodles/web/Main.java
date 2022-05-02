d1ec7a1f4a63a3936f2d9959a077369103e45ecd
huchangbin <huchangbin@yuanfudao.com>
2022-05-03 01:21:12 +0800

    ADD："制造合并分支的冲突"

--- src/main/java/com/noodles/web/Main.java
+++ src/main/java/com/noodles/web/Main.java
@@ -18,7 +18,7 @@ public class Main {
         double res = a / b;
         if(b == 0){
             System.out.println("除数不能为零");
-            throw new ArithmeticException("除数不能为零");
+            throw new ArithmeticException("除数11111不能为零");
         }
         return res;
     }
