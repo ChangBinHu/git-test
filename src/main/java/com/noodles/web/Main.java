d1d02eeeaa1caeb14d81d954e50cbcc51148311c
huchangbin <huchangbin@yuanfudao.com>
2022-05-02 15:13:30 +0800

    ADD:两个数相除

---
+++ src/main/java/com/noodles/web/Main.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)Main.java, 5月 02, 2022.
+ * <p>
+ * Copyright 2022 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.noodles.web;
+
+/**
+ * @author huchangbin
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int sum = a+b;
+    }
+}
